afae29aa4f7bc431bc1cc700900013b703f70f4e
Tunde Agboola <tun.agboola@gmail.com>
2024-05-22 14:10:31 -0400

    Improve chat UX

--- src/app/components/AgentChatMessage.tsx
+++ src/app/components/AgentChatMessage.tsx
@@ -1,12 +1,32 @@
+import Stack from "@mui/material/Stack";
 import { Message } from "../types";
+import LinearProgress from "@mui/material/LinearProgress";
+import SupportAgentIcon from "@mui/icons-material/SupportAgent";
 
 export interface AgentChatMessageProps {
   message: Message;
 }
 export function AgentChatMessage({ message }: AgentChatMessageProps) {
-  if (message.value && message.value.length > 0) {
-    return <div className="chat-message-container">{message.value}</div>;
-  } else {
-    return <div className="chat-message-container loading">Agent-ting</div>;
+  function renderMessage() {
+    if (message.value && message.value.length > 0) {
+      return <div className="chat-message-container">{message.value}</div>;
+    } else {
+      return (
+        <Stack sx={{ width: "100%", color: "grey.500" }} spacing={2}>
+          <LinearProgress color="secondary" />
+          <LinearProgress color="success" />
+          <LinearProgress color="inherit" />
+        </Stack>
+      );
+    }
   }
+
+  return (
+    <div className="chat-message-container">
+      <div className="chat-message-icon-container">
+        <SupportAgentIcon fontSize="large"></SupportAgentIcon>
+      </div>
+      <div className="chat-message-content-container">{renderMessage()}</div>
+    </div>
+  );
 }
--- src/app/components/UserChatMessage.tsx
+++ src/app/components/UserChatMessage.tsx
@@ -1,8 +1,17 @@
+import Icon from "@mui/material/Icon";
 import { Message } from "../types";
+import Person2OutlinedIcon from "@mui/icons-material/Person2Outlined";
 
 export interface UserChatMessage {
   message: Message;
 }
 export function UserChatMessage({ message }: UserChatMessage) {
-  return <div className="chat-message-container">{message.value}</div>;
+  return (
+    <div className="chat-message-container">
+      <div className="chat-message-icon-container">
+        <Person2OutlinedIcon fontSize="large"></Person2OutlinedIcon>
+      </div>
+      <div className="chat-message-content-container">{message.value}</div>
+    </div>
+  );
 }
