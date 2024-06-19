37f6d7301b0f48bcb74c416478335baedf119e3d
Michael Doyle <michaeldoyle@google.com>
2024-06-18 21:44:16 -0400

    Increase OTEL max queue size and re-enable metrics

--- src/app/genkit.ts
+++ src/app/genkit.ts
@@ -37,11 +37,11 @@ configureGenkit({
       telemetryConfig: {
         sampler: new AlwaysOnSampler(),
         autoInstrumentation: true,
-        // autoInstrumentationConfig: {
-        //   "@opentelemetry/instrumentation-fs": { enabled: false },
-        //   "@opentelemetry/instrumentation-dns": { enabled: false },
-        //   "@opentelemetry/instrumentation-net": { enabled: false },
-        // },
+        autoInstrumentationConfig: {
+          "@opentelemetry/instrumentation-fs": { enabled: false },
+          "@opentelemetry/instrumentation-dns": { enabled: false },
+          "@opentelemetry/instrumentation-net": { enabled: false },
+        },
       },
     }),
   ],
